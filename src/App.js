59f05a08d172c43e37776eb42a3e8a783bd4bfcb
jseneris <johnseneris@gmail.com>
2017-11-09 17:32:21 -0500

    Initial file setup for CodeAcademy Jamming project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+<div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+    <!-- Add a SearchBar component -->
+    <div class="App-playlist">
+      <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component -->
+    </div>
+  </div>
+</div>
